dbe9bdac3486230be33d4916e6105eedc665faa1
DIMANVAZ <DIMANVAZ04@GMAIL.COM>
2022-09-30 23:43:49 +0600

    some more exercises done

--- Yura.py
+++ Yura.py
@@ -114,4 +114,55 @@ print(hf(4))
 def truncate(string, remainedLength):
     return string[:remainedLength] + '...'
 
-print(truncate('hahaha',2))
+
+print(truncate('hahaha', 2))
+
+
+def get_hidden_card(string, starsamount=4):  # "*1100"
+    stars = "*" * starsamount
+    cutted = string[-4:]
+    print(cutted)
+    return stars + cutted
+
+
+print(get_hidden_card('12345678912', 5))
+
+
+def named(a, b, c=7):
+    print(a / b + c)
+
+
+named(b=4, a=12)
+
+
+def trim_and_repeat(text, offset=0, repetitions=1):
+    return text[offset:] * repetitions
+
+
+print(trim_and_repeat('python', offset=3, repetitions=2))
+
+
+def get_age_difference(year1, year2):
+    return f'The age difference is {abs(year2 - year1)}'
+
+
+print(get_age_difference(2020, 1009))
+
+
+def has_upper_case(word):
+    return word.lower() != word
+
+print(has_upper_case('Words'))
+print(has_upper_case('woed'))
+print(has_upper_case(''))
+
+def is_leap_year(year):
+    if year % 400 == 0:
+        return True
+    if year % 4 == 0 and year % 100 != 0:
+        return True
+    return False
+
+print(is_leap_year(2018))
+print(is_leap_year(2017))
+print(is_leap_year(2016))
